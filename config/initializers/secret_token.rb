7bc2840ddda3c7545e170fd02cbd66bc8175161a
Aleksandra Bondzic <abondzic@enova.com>
2014-01-06 22:30:37 -0600

    initial new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheGreyGirl::Application.config.secret_key_base = '62d5490cc43558b1357c8f84d5be73c99b768b16c65265498e00f65c8f352a99018ed9e7718ecc905a8e11108c003a75a904a2b394320e676fa5a5085b49aee4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheGreyGirl::Application.config.session_store :cookie_store, key: '_the_grey_girl_session'
