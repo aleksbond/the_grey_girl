cbc03ba60d044dded1853a8173d0985ef6d1007a
Aleksandra Bondzic <abondzic@enova.com>
2014-12-10 02:03:57 -0600

    Added more stuff for images

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ActiveRecord::Base
+  has_many :images
   paginates_per 1
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803185236) do
+ActiveRecord::Schema.define(version: 20141210075132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20140803185236) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
 end
